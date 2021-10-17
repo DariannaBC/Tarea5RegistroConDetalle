8b0bdca6df69be5c93db4b3af2ce6b71bd6766e0
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-17 18:33:59 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea5RegistroConDetalle.Entidades;
+
+namespace Tarea5RegistroConDetalle.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Permisos> Permiso { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data source = DATA\rolescontrol.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Permisos>().HasData(
+                new Permisos { PermisoId = 1, Nombre = "Usuario", Descripcion = "Para usuarios" },
+                new Permisos { PermisoId = 2, Nombre = "Administrador", Descripcion = "Para administradores" });
+
+        }
+    }
+}
